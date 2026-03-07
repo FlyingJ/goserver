6668a5a0d52aecb43fa94d84e59d1d151e463408
Jason Harrington <jason.m.harrington@gmail.com>
2026-03-07 01:03:30 -0500

    Add unit testing for response utility functions

---
+++ internal/util/response_test.go
@@ -0,0 +1,16 @@
+package util_test
+
+import (
+	"testing"
+
+	"github.com/FlyingJ/goserver/internal/util"
+)
+
+func TestRespondWithJSON(t *testing.T) {
+	// code
+}
+
+func TestRespondWithError(t *testing.T) {
+	// code
+}
+
